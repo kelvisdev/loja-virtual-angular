2b5e12c082155fccfd424016966b90f0fae8c0a3
Kelvis Borges <kelvisbcc@gmail.com>
2021-03-12 22:24:37 -0300

    Cria module para navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app.routes';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { CadastroComponent } from './demos/reactive-forms/cadastro/cadastro.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
 
-const routes: Routes = rootRouterConfig;
+const routes: Routes = [
+  { path: '', redirectTo:'/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'sobre', component: HomeComponent },
+  { path: 'feature-data-binding', component: DataBindingComponent },
+  { path: 'produtos', component: ListaProdutoComponent },
+  { path: 'cadastro', component: CadastroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
+import { NavegacaoModule } from './navegacao/navegacao.module';
 import { ProdutoService } from './produtos/produtos.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { APP_BASE_HREF } from '@angular/common';
@@ -25,9 +23,7 @@ registerLocaleData(localPt);
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
+  
     SobreComponent,
     ContatoComponent,
     DataBindingComponent,
@@ -38,6 +34,7 @@ registerLocaleData(localPt);
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    NavegacaoModule,
     HttpClientModule,
     ReactiveFormsModule,
     TextMaskModule,
