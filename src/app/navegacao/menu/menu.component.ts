38572300c20f31eba65f0d3a3ac0c8c6d2c50d0e
Kelvis Borges <kelvisbcc@gmail.com>
2021-03-14 10:39:49 -0300

    Adiciona menus dinamicos

--- src/app/navegacao/menu/menu.component.ts
+++ src/app/navegacao/menu/menu.component.ts
@@ -1,7 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface NavProps {
+  link: string;
+  name: string;
+  exact: boolean;
+  admin: boolean;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {}
+export class MenuComponent {
+  nav: NavProps[] = [
+    { 
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    { 
+      link: '/cadastro',
+      name: 'Cadastro',
+      exact: true,
+      admin: false
+    },
+    { 
+      link: '/sobre',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    { 
+      link: '/produtos',
+      name: 'Produtos',
+      exact: true,
+      admin: false
+    }
+  ];
+}
+
